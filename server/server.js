2de094fc2b2bbf958573bf28167a74e928b31b5b
khushialodhan17 <khushialodhan17@gmail.com>
2025-06-30 00:02:39 +0530

    error solved

--- server/server.js
+++ server/server.js
@@ -7,12 +7,15 @@ import { clerkMiddleware } from '@clerk/express'
 
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
 
 const app = express();
 const port = 3000;
 
 
 await connectDB()
+
+
 
 // Middleware
 app.use(express.json());
@@ -24,5 +27,6 @@ app.use(clerkMiddleware())
 app.get('/', (req, res) => res.send('Server is Live!'));
 
 app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/show',showRouter)
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
